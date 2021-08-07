8867faa0366da00e28a15a1017404b3a9ab93685
Varad Gauthankar <gvarad1601@gmail.com>
2021-08-07 17:09:52 +0530

    updted article model

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -1,50 +1,35 @@
+import 'package:noobs2pro_app/models/media.dart';
+
 class Article {
   int? id;
   String? date;
-  String? dateGmt;
   String? link;
   String? title;
   String? content;
-  String? excerpt;
-  int? featuredMedia;
-  List<int>? categories;
+  String? shortContent;
+  String? category;
+  Media? featuredMedia;
 
   Article({
     this.id,
     this.date,
-    this.dateGmt,
     this.link,
     this.title,
     this.content,
-    this.excerpt,
+    this.shortContent,
+    this.category,
     this.featuredMedia,
-    this.categories,
   });
 
   Article.fromJson(Map<String, dynamic> json) {
     id = json['id'] as int;
     date = json['date'] as String;
-    dateGmt = json['date_gmt'] as String;
     link = json['link'] as String;
     title = json['title']['rendered'] as String;
     content = json['content']['rendered'] as String;
-    excerpt = json['excerpt']['rendered'] as String;
-    categories = json['categories'].cast<int>() as List<int>;
-    featuredMedia = json['featured_media'] as int;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = Map<String, dynamic>();
-    data['id'] = id;
-    data['date'] = date;
-    data['date_gmt'] = dateGmt;
-    data['link'] = link;
-    data['title'] = title;
-    data['content'] = content;
-    data['excerpt'] = excerpt;
-    data['featured_media'] = featuredMedia;
-    data['categories'] = categories;
-
-    return data;
+    shortContent = json['excerpt']['rendered'] as String;
+    featuredMedia = Media.fromJson(
+        json['_embedded']['wp:featuredmedia'][0] as Map<String, dynamic>);
+    category = json['_embedded']['wp:term'][0][0]['name'] as String;
   }
 }
