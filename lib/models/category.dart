b46542cf3a7ad4b64934d7986a62fdc06246146f
Varad Gauthankar <gvarad1601@gmail.com>
2021-08-07 21:56:01 +0530

    added id to category

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,16 +1,20 @@
 class Category {
+  int? id;
   String? name;
 
   Category({
+    this.id,
     this.name,
   });
 
   Category.fromJson(Map<String, dynamic> json) {
+    id = json['id'] as int;
     name = json['name'] as String;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
     data['name'] = name;
     return data;
   }
