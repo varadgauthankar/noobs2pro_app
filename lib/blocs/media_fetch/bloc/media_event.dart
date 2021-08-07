208871b7ec23a513fc28b25105e78eff0a0e7fe7
Varad Gauthankar <gvarad1601@gmail.com>
2021-08-07 21:53:58 +0530

    delete unused stuff

--- lib/blocs/media_fetch/bloc/media_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'media_bloc.dart';
-
-@immutable
-abstract class MediaEvent {}
-
-class FetchMediaEvent extends MediaEvent {
-  final int mediaId;
-  FetchMediaEvent(this.mediaId);
-}
--- lib/blocs/media_fetch/repository/media_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:noobs2pro_app/models/media.dart';
-
-abstract class MediaReposiotry {
-  Future<Media> fetchMedia(int mediaId);
-}
