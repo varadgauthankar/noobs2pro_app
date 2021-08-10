46f0c9ea013a9b893e35b0f33c0546d213b8ea49
Varad Gauthankar <gvarad1601@gmail.com>
2021-08-11 00:05:37 +0530

    added poppins font

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -3,10 +3,11 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:noobs2pro_app/utils/colors.dart';
 
 const TextStyle articleTitle = TextStyle(
-    fontSize: 18.0,
-    decoration: TextDecoration.none,
-    fontFamily: 'OpenSans',
-    fontWeight: FontWeight.w600);
+  fontSize: 18.0,
+  decoration: TextDecoration.none,
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.w400,
+);
 
 Style aTagStyle = Style(
   color: kAccentColor,
