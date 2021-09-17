ccb5382c1b80cef81e9f56951c94bfff31387169
Varad Gauthankar <gvarad1601@gmail.com>
2021-09-17 21:23:41 +0530

    updated drawer

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:noobs2pro_app/blocs/articles_fetch/bloc/articles_bloc.dart';
 import 'package:noobs2pro_app/blocs/articles_fetch/repository/articles_repository_impl.dart';
 import 'package:noobs2pro_app/blocs/theme_bloc/bloc/bloc/theme_bloc.dart';
@@ -10,6 +11,8 @@ import 'package:noobs2pro_app/pages/category_articles_page.dart';
 import 'package:noobs2pro_app/pages/home_page.dart';
 import 'package:noobs2pro_app/pages/pages.dart';
 import 'package:noobs2pro_app/services/firebase_auth.dart';
+import 'package:noobs2pro_app/services/hive_service.dart';
+import 'package:noobs2pro_app/utils/colors.dart';
 import 'package:noobs2pro_app/utils/helpers.dart';
 import 'package:noobs2pro_app/utils/text_styles.dart';
 
@@ -111,105 +114,127 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: const CircleAvatar(),
-              accountName: Text(kAppName),
-              accountEmail: const Text('version: 1.0'),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 10.0),
-              child: Text(
-                'CATEGORIES',
-                style: categoryTitle,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 4.0),
+                color: kPrimaryColor,
+                child: Image.asset('assets/images/logo_full.png'),
               ),
-            ),
-            Column(
-              children: categories
-                  .map((e) => ListTile(
-                        visualDensity: const VisualDensity(vertical: -4),
-                        title: Text(
-                          e.name!,
-                          style: categoryItems,
-                        ),
-                        onTap: () => goToPage(
-                            context,
-                            CategoryArticlesPage(
-                              categoryTitle: e.name!,
-                              categoryId: e.id!,
-                            )),
-                      ))
-                  .toList(),
-            ),
-            const Divider(),
-            Column(
-              children: [
-                SwitchListTile(
-                    title: const Text(
-                      'Dark Mode',
-                      style: categoryItems,
+              Expanded(
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0, top: 10.0),
+                      child: Text(
+                        'CATEGORIES',
+                        style: categoryTitle,
+                      ),
                     ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: categories
+                          .map(
+                            (e) => ListTile(
+                              visualDensity: const VisualDensity(vertical: -4),
+                              title: Text(
+                                e.name!,
+                                style: categoryItems,
+                              ),
+                              onTap: () => goToPage(
+                                context,
+                                CategoryArticlesPage(
+                                  categoryTitle: e.name!,
+                                  categoryId: e.id!,
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+              InkWell(
+                onTap: () {
+                  BlocProvider.of<ThemeBloc>(context).add(ThemeChanged(
+                    isThemeDark(context) ? ThemeMode.light : ThemeMode.dark,
+                  ));
+                },
+                child: ListTile(
+                  visualDensity: const VisualDensity(vertical: -4),
+                  title: const Text(
+                    'Dark Mode',
+                    style: categoryItems,
+                  ),
+                  trailing: Switch(
                     value: isThemeDark(context),
-                    onChanged: (val) {
+                    onChanged: (_) {
                       BlocProvider.of<ThemeBloc>(context).add(ThemeChanged(
                         isThemeDark(context) ? ThemeMode.light : ThemeMode.dark,
                       ));
-                    }),
-                const Divider(),
-                ListTile(
-                  // leading: Icon(EvaIcons.logOut),
-                  visualDensity: const VisualDensity(vertical: -4),
-                  trailing: const Icon(EvaIcons.logOut),
-                  title: const Text(
-                    'LOG OUT',
-                    style: categoryItems,
+                    },
                   ),
-                  selected: true,
-                  onTap: () {
-                    // TODO: improve this
-                    // TODO: clear hive box after logout
-                    Navigator.pop(context);
-
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Log out?'),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('NO'),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  FirebaseAuthService().signOut().then(
-                                    (_) {
-                                      Navigator.pushAndRemoveUntil(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                const AuthMainPage(),
-                                          ),
-                                          (route) => false);
-                                    },
-                                  );
-                                },
-                                child: const Text('YES'),
-                              ),
-                            ],
-                          );
-                        });
-                  },
                 ),
-              ],
-            ),
-          ],
+              ),
+              const Divider(),
+              ListTile(
+                visualDensity: const VisualDensity(vertical: -4),
+                trailing: const Icon(EvaIcons.logOut),
+                title: const Text(
+                  'LOG OUT',
+                  style: categoryItems,
+                ),
+                selected: true,
+                onTap: () {
+                  // TODO: improve this
+
+                  Navigator.pop(context);
+
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Log out?'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('NO'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              FirebaseAuthService().signOut().then(
+                                (_) {
+                                  HiveService().allArticleBox.clear();
+                                  Navigator.pushAndRemoveUntil(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const AuthMainPage(),
+                                      ),
+                                      (route) => false);
+                                },
+                              );
+                            },
+                            child: const Text('YES'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
       body: BlocProvider(
